992fe09a7444b88b765d396350c440aa6d4e04cc
soh1945 <san4yone3@gmail.com>
2018-01-30 17:45:05 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -38,189 +38,189 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	//char filename[100];
-	//for (int i = 0; i < 30; i++)
-	//{
-	//	sprintf_s(filename, "c:/data/test/test_%d.jpg", i);
-	//	Mat img = imread(filename);
-	//	if (img.empty())
-	//	{
-	//		return(-1);
-	//	}
-	//	cout << filename << endl;
+	char filename[100];
+	for (int i = 0; i < 30; i++)
+	{
+		sprintf_s(filename, "c:/data/test/test_%d.jpg", i);
+		Mat img = imread(filename);
+		if (img.empty())
+		{
+			return(-1);
+		}
+		cout << filename << endl;
 
 
-	//	Mat gray_img, blur_img, bin_img;
-	//	// ƒOƒŒ[ƒXƒP[ƒ‹
-	//	cvtColor(img, gray_img, CV_BGR2GRAY);
-	//	// ƒKƒEƒVƒAƒ“ƒtƒBƒ‹ƒ^
-	//	GaussianBlur(gray_img, blur_img, Size(9, 9), 0);
-	//	// “ñ’l‰»
-	//	threshold(blur_img, bin_img, 250, 255, cv::THRESH_BINARY_INV);
-	//	//ƒ‰‚×ƒŠƒ“ƒOˆ—
-	//	Mat labelImage(img.size(), CV_32S);
-	//	int nLabels = connectedComponents(bin_img, labelImage, 4);
-	//	nLabels = nLabels - 1;
-	//	//std::cout << nLabels  << std::endl;
+		Mat gray_img, blur_img, bin_img;
+		// ã‚°ãƒ¬ãƒ¼ã‚¹ã‚±ãƒ¼ãƒ«
+		cvtColor(img, gray_img, CV_BGR2GRAY);
+		// ã‚¬ã‚¦ã‚·ã‚¢ãƒ³ãƒ•ã‚£ãƒ«ã‚¿
+		GaussianBlur(gray_img, blur_img, Size(9, 9), 0);
+		// äºŒå€¤åŒ–
+		threshold(blur_img, bin_img, 250, 255, cv::THRESH_BINARY_INV);
+		//ãƒ©ã¹ãƒªãƒ³ã‚°å‡¦ç†
+		Mat labelImage(img.size(), CV_32S);
+		int nLabels = connectedComponents(bin_img, labelImage, 4);
+		nLabels = nLabels - 1;
+		//std::cout << nLabels  << std::endl;
 
-	//	//•¡”
-	//	if (nLabels > 1)
-	//	{
-	//		//imwrite("c:/data/multi/test_10.jpg", img);
-	//		std::cout << "•¡”" << std::endl;
-	//		namedWindow("bin", WINDOW_NORMAL);
-	//		imshow("bin", bin_img);
-	//		waitKey(0);
-	//		destroyAllWindows();
-	//		continue;
-	//	}
-
-
-	//	// ‰¡‚ÌƒsƒNƒZƒ‹”
-	//	binSum = 0, binSum2 = 0;
-	//	for (int y = 0; y < img.rows; y++)
-	//	{
-	//		for (int x = 0; x < img.cols; x++)
-	//		{
-	//			bin = bin_img.at<unsigned char>(y, x);
-	//			bin = bin / 255;
-	//			if (bin == 1)
-	//			{
-	//				binSum++;
-	//				pointY = y;
-	//				pointX = x;
-	//			}
-	//		}
-	//		if (binSum > binSum2)
-	//		{
-	//			binSum2 = binSum;
-	//			binSum = 0;
-	//			widthArray[0][0] = pointY;
-	//			widthArray[0][1] = pointX;
-	//		}
-	//		else {
-	//			binSum = 0;
-	//		}
-
-	//	}
-	//	width = binSum2;
-
-	//	//@c‚ÌƒsƒNƒZƒ‹”
-	//	binSum = 0, binSum2 = 0;
-	//	for (int x = 0; x < img.cols; x++)
-	//	{
-
-	//		for (int y = 0; y < img.rows; y++)
-	//		{
-	//			bin = bin_img.at<unsigned char>(y, x);
-	//			bin = bin / 255;
-	//			if (bin == 1)
-	//			{
-	//				binSum++;
-	//				pointY = y;
-	//				pointX = x;
-
-	//			}
-	//		}
-	//		if (binSum > binSum2)
-	//		{
-	//			binSum2 = binSum;
-	//			binSum = 0;
-	//			heightArray[0][0] = pointY;
-	//			heightArray[0][1] = pointX;
-	//		}
-	//		else {
-	//			binSum = 0;
-	//		}
-
-	//	}
-	//	height = binSum2;
-
-	//	//ŒC‰º‚Ìx‚Æy‚Ì“ñ“_ŠÔ‚ğ‹‚ß‚é
-	//	double x1, y1, x2, y2, mx, my, dist;
-	//	x1 = heightArray[0][1];
-	//	y1 = widthArray[0][0];
-	//	x2 = heightArray[0][1];
-	//	y2 = heightArray[0][0];
-	//	mx = abs(x2 - x1);
-	//	my = abs(y2 - y1);
-	//	dist = sqrt(pow(mx, 2) + pow(my, 2));
-	//	dist = dist / img.rows;
-
-	//	double heightD = static_cast<double>(height);
-	//	double widthD = static_cast<double>(width);
-
-	//	std::cout << "‰¡" << width << std::endl;
-	//	std::cout << "c" << height << std::endl;
-	//	std::cout << heightD / img.rows << std::endl;
-	//	std::cout << widthD / img.cols << std::endl;
-	//	std::cout << "y" << widthArray[0][0] << std::endl;
-	//	std::cout << "x" << widthArray[0][1] << std::endl;
-	//	std::cout << "y" << heightArray[0][0] << std::endl;
-	//	std::cout << "x" << heightArray[0][1] << std::endl;
-	//	std::cout << "“ñ“_ŠÔ" << dist << std::endl;
-
-	//	//ŒC‰º
-	//	if ((heightD / img.cols) >= 0.6 && (widthD / img.rows) <= 0.37 && dist <= 0.25)
-	//	{
-	//		std::cout << "ŒC‰º" << std::endl;
-	//	}
-	//	//ã’…
-	//	else if (1 <= (heightD / widthD) && (heightD / widthD) < 3.1)
-	//	{
-	//		min = 0, min2 = 0;
-	//		for (int i = 0; i < 5; i++) {
-	//			binSum = 0;
-	//			binSum2 = 0;
-	//			for (int j = 0; j < img.size().height; j++) {
-	//				xa = img.cols / 2 + i;
-	//				xb = img.cols / 2 - i;
-	//				bin = bin_img.at<unsigned char>(j, xa);
-	//				bin2 = bin_img.at<unsigned char>(j, xb);
-	//				bin = bin / 255;
-	//				bin2 = bin2 / 255;
-	//				if (bin == 1) {
-	//					binSum++;
-	//				}
-	//				if (bin2 == 1) {
-	//					binSum2++;
-	//				}
-	//				if (binSum > min) {
-	//					min = binSum;
-	//				}
-	//				if (binSum2 > min2) {
-	//					min2 = binSum2;
-	//				}
-
-	//				if (min > min2) {
-	//					min = min2;
-	//				}
-	//			}
-	//		}
-	//		if (min >= img.rows * 0.8) {
-
-	//			cout << "ã’…AƒXƒJ[ƒg" << endl;
-	//		}
-	//		else {
-	//			cout << "’·ƒYƒ{ƒ“" << endl;
-	//		}
-	//	}
-	//	//ƒpƒ“ƒc
-	//	else if (0.5 <= (heightD / widthD) && (heightD / widthD) < 1)
-	//	{
-	//		std::cout << "ƒpƒ“ƒc" << std::endl;
-	//	}
-	//	//‚»‚Ì‘¼
-	//	else {
-	//		std::cout << "‚»‚Ì‘¼" << std::endl;
-	//	}
+		//è¤‡æ•°
+		if (nLabels > 1)
+		{
+			//imwrite("c:/data/multi/test_10.jpg", img);
+			std::cout << "è¤‡æ•°" << std::endl;
+			namedWindow("bin", WINDOW_NORMAL);
+			imshow("bin", bin_img);
+			waitKey(0);
+			destroyAllWindows();
+			continue;
+		}
 
 
-	//	namedWindow("bin", WINDOW_NORMAL);
-	//	imshow("bin", bin_img);
-	//	waitKey(0);
-	//	destroyAllWindows();
-	//}
+		// æ¨ªã®ãƒ”ã‚¯ã‚»ãƒ«æ•°
+		binSum = 0, binSum2 = 0;
+		for (int y = 0; y < img.rows; y++)
+		{
+			for (int x = 0; x < img.cols; x++)
+			{
+				bin = bin_img.at<unsigned char>(y, x);
+				bin = bin / 255;
+				if (bin == 1)
+				{
+					binSum++;
+					pointY = y;
+					pointX = x;
+				}
+			}
+			if (binSum > binSum2)
+			{
+				binSum2 = binSum;
+				binSum = 0;
+				widthArray[0][0] = pointY;
+				widthArray[0][1] = pointX;
+			}
+			else {
+				binSum = 0;
+			}
+
+		}
+		width = binSum2;
+
+		//ã€€ç¸¦ã®ãƒ”ã‚¯ã‚»ãƒ«æ•°
+		binSum = 0, binSum2 = 0;
+		for (int x = 0; x < img.cols; x++)
+		{
+
+			for (int y = 0; y < img.rows; y++)
+			{
+				bin = bin_img.at<unsigned char>(y, x);
+				bin = bin / 255;
+				if (bin == 1)
+				{
+					binSum++;
+					pointY = y;
+					pointX = x;
+
+				}
+			}
+			if (binSum > binSum2)
+			{
+				binSum2 = binSum;
+				binSum = 0;
+				heightArray[0][0] = pointY;
+				heightArray[0][1] = pointX;
+			}
+			else {
+				binSum = 0;
+			}
+
+		}
+		height = binSum2;
+
+		//é´ä¸‹ã®xã¨yã®äºŒç‚¹é–“ã‚’æ±‚ã‚ã‚‹
+		double x1, y1, x2, y2, mx, my, dist;
+		x1 = heightArray[0][1];
+		y1 = widthArray[0][0];
+		x2 = heightArray[0][1];
+		y2 = heightArray[0][0];
+		mx = abs(x2 - x1);
+		my = abs(y2 - y1);
+		dist = sqrt(pow(mx, 2) + pow(my, 2));
+		dist = dist / img.rows;
+
+		double heightD = static_cast<double>(height);
+		double widthD = static_cast<double>(width);
+
+		std::cout << "æ¨ª" << width << std::endl;
+		std::cout << "ç¸¦" << height << std::endl;
+		std::cout << heightD / img.rows << std::endl;
+		std::cout << widthD / img.cols << std::endl;
+		std::cout << "y" << widthArray[0][0] << std::endl;
+		std::cout << "x" << widthArray[0][1] << std::endl;
+		std::cout << "y" << heightArray[0][0] << std::endl;
+		std::cout << "x" << heightArray[0][1] << std::endl;
+		std::cout << "äºŒç‚¹é–“" << dist << std::endl;
+
+		//é´ä¸‹
+		if ((heightD / img.cols) >= 0.6 && (widthD / img.rows) <= 0.37 && dist <= 0.25)
+		{
+			std::cout << "é´ä¸‹" << std::endl;
+		}
+		//ä¸Šç€
+		else if (1 <= (heightD / widthD) && (heightD / widthD) < 3.1)
+		{
+			min = 0, min2 = 0;
+			for (int i = 0; i < 5; i++) {
+				binSum = 0;
+				binSum2 = 0;
+				for (int j = 0; j < img.size().height; j++) {
+					xa = img.cols / 2 + i;
+					xb = img.cols / 2 - i;
+					bin = bin_img.at<unsigned char>(j, xa);
+					bin2 = bin_img.at<unsigned char>(j, xb);
+					bin = bin / 255;
+					bin2 = bin2 / 255;
+					if (bin == 1) {
+						binSum++;
+					}
+					if (bin2 == 1) {
+						binSum2++;
+					}
+					if (binSum > min) {
+						min = binSum;
+					}
+					if (binSum2 > min2) {
+						min2 = binSum2;
+					}
+
+					if (min > min2) {
+						min = min2;
+					}
+				}
+			}
+			if (min >= img.rows * 0.8) {
+
+				cout << "ä¸Šç€ã€ã‚¹ã‚«ãƒ¼ãƒˆ" << endl;
+			}
+			else {
+				cout << "é•·ã‚ºãƒœãƒ³" << endl;
+			}
+		}
+		//ãƒ‘ãƒ³ãƒ„
+		else if (0.5 <= (heightD / widthD) && (heightD / widthD) < 1)
+		{
+			std::cout << "ãƒ‘ãƒ³ãƒ„" << std::endl;
+		}
+		//ãã®ä»–
+		else {
+			std::cout << "ãã®ä»–" << std::endl;
+		}
+
+
+		namedWindow("bin", WINDOW_NORMAL);
+		imshow("bin", bin_img);
+		waitKey(0);
+		destroyAllWindows();
+	}
 
 	return(0);
 }
